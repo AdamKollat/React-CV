ec7c09c0abb406bb265038b4590c836f8f177147
Adam <adam.kollat@gmail.com>
2023-01-15 17:34:23 +0000

    Completed initial profile layout

---
+++ src/Components/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet. Vel voluptatem facere et facere numquam eos vero ullam aut deserunt autem est maiores reprehenderit. Vel esse aperiam est libero dolore sed minus iure!</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/Components/aside.js
@@ -0,0 +1,14 @@
+const Profile = () => {
+    return (
+        <div class='profile'>
+            {/* {image} */}
+            <h1>Adam Kollat</h1>
+            <h2>Front End Developer</h2>
+            <hr></hr>
+            <h4>Fareham, United Kingdom</h4>
+            <a href="mailto:adam.kollat@gmail.com"><h4>adam.kollat@gmail.com</h4></a>
+        </div>
+    )
+}
+
+export default Profile;
