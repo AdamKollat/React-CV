178f2278edec10c5c14562676a9cc48c3dc3c3a7
Adam <adam.kollat@gmail.com>
2023-01-16 19:46:31 +0000

    Updated work experience component copy

--- src/Components/work_experience.js
+++ src/Components/work_experience.js
@@ -6,7 +6,7 @@ const WorkExperience = () => {
             <h4> Martech Specialist / Front End Developer</h4>
             <h4>Clevertouch Marketing</h4>
             <p class='grey'>09/2021 - Present</p>
-            <p>Lorem ipsum dolor sit amet. Vel voluptatem facere et facere numquam eos vero ullam aut deserunt autem est maiores reprehenderit. Vel esse aperiam est libero dolore sed minus iure!</p>
+            <p>I joined Clevertouch Marketing as a Martech Specialist in late 2021. This position has helped me to turn my theoretical knowledge to practical experience in the field of front end web development. My duties included building HTML emails and Landing pages for our clients (such as Orange Business Services and Fujitsu) along with building and maintaining marketing campaigns using marketing automation platforms. I have also been involved in voluntary development projects within the tech delivery team. One such project required my team to integrate two scheduling platforms using APIs. The aim of the project was to build a process that automatically syncs holiday bookings from our staff holiday planner to our scheduling platform (Adobe Workfront). We successfully delivered the project in 3 months mostly working on it in our spare time.</p>
         </div>
     )
 }
